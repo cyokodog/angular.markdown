680148a39fcdc85c04e713de2cd9d5549957f6da
cyokodog <cyokodog@gmail.com>
2015-06-26 00:27:40 +0900

    $inject追記

--- angular.markdown.js
+++ angular.markdown.js
@@ -18,6 +18,7 @@
       },
       controller: controller
     }
+    controller.$inject = ['$compile', 'mdService'];
     function controller($compile, mdService){
       this.init = function(scope, el, attr){
         var url = scope.url;
@@ -36,6 +37,7 @@
 //markdown.md-fetch.sevice.js
 ;(function(){
   angular.module('angular-markdown').factory('mdService', factory);
+  factory.$inject = ['$http', '$q'];
   function factory($http, $q){
     return {
       fetch: function(url){
