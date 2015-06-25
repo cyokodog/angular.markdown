cb473e07354b2985e873405f70267f4b2b2ddef4
cyokodog <cyokodog@gmail.com>
2015-06-26 00:23:36 +0900

    first commit

---
+++ angular.markdown.js
@@ -0,0 +1,51 @@
+//markdown.module.js
+;(function(){
+  angular.module('angular-markdown', []);
+})();
+
+//markdown.md-loader.directive.js
+;(function(){
+  angular.module('angular-markdown').directive('mdLoader', directiveFactory);
+  function directiveFactory(){
+    return {
+      scope: {
+        url: '@',
+        ret: '=',
+        mdCallback: '&'
+      },
+      link: function(scope, el, attr, vm){
+        vm.init(scope, el, attr);
+      },
+      controller: controller
+    }
+    function controller($compile, mdService){
+      this.init = function(scope, el, attr){
+        var url = scope.url;
+        if(url){
+          var promise = mdService.fetch(url).then(function(res){
+            el.html(marked(res))
+            $compile(el.contents())(scope);
+          });
+          scope.mdCallback({promise: promise});
+        }
+      }
+    }
+  }
+})();
+
+//markdown.md-fetch.sevice.js
+;(function(){
+  angular.module('angular-markdown').factory('mdService', factory);
+  function factory($http, $q){
+    return {
+      fetch: function(url){
+        var d = $q.defer();
+        $http.get(url).
+        success(function(res){
+          d.resolve(res);
+        });
+        return d.promise;
+      }
+    }
+  }
+})();
